08a953003baeb4b39dce5edb1b88e441136f042b
Mark Bates <mark@markbates.com>
2016-12-02 11:31:16 -0500

    proved what i wanted to with travis, fixing the tests again

--- route_test.go
+++ route_test.go
@@ -32,7 +32,7 @@ func Test_App_Routes_with_Root(t *testing.T) {
 	g.GET("/foo", voidHandler)
 
 	routes := a.Routes()
-	r.Len(routes, 2)
+	r.Len(routes, 1)
 	route := routes[0]
 	r.Equal("GET", route.Method)
 	r.Equal("/api/v1/foo", route.Path)
