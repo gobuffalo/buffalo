b0dcbc1187e7594ba68936ec247c758c8505b00a
Mark Bates <mark@markbates.com>
2016-12-01 22:05:23 -0500

    let's hope this does it!

--- buffalo/cmd/new.go
+++ buffalo/cmd/new.go
@@ -88,17 +88,17 @@ func installDeps(pwd string, rootPath string) error {
 	cmds := []*exec.Cmd{
 		exec.Command("go", "get", "-u", "-v", "github.com/Masterminds/glide"),
 		exec.Command("glide", "init", "--non-interactive"),
-		exec.Command("glide", "get", "-v", "-u", "--non-interactive", "github.com/markbates/refresh"),
+		exec.Command("glide", "get", "-v", "-u", "--non-interactive", "--all-dependencies", "github.com/markbates/refresh"),
 		exec.Command("go", "install", "-v", "./vendor/github.com/markbates/refresh"),
-		exec.Command("glide", "get", "-v", "-u", "--non-interactive", "github.com/markbates/grift"),
+		exec.Command("glide", "get", "-v", "-u", "--non-interactive", "--all-dependencies", "github.com/markbates/grift"),
 		exec.Command("go", "install", "-v", "./vendor/github.com/markbates/grift"),
 		exec.Command("refresh", "init"),
 	}
 
 	if !skipPop {
 		cmds = append(cmds,
-			exec.Command("glide", "get", "-v", "-u", "--non-interactive", "github.com/markbates/pop"),
-			exec.Command("glide", "get", "-v", "-u", "--non-interactive", "github.com/markbates/pop/soda"),
+			exec.Command("glide", "get", "-v", "-u", "--non-interactive", "--all-dependencies", "github.com/markbates/pop"),
+			exec.Command("glide", "get", "-v", "-u", "--non-interactive", "--all-dependencies", "github.com/markbates/pop/soda"),
 			exec.Command("go", "install", "-v", "./vendor/github.com/markbates/pop/soda"),
 			exec.Command("soda", "g", "config", "-t", dbType),
 		)
