661141f49043e7aeed47a5061018358a43aa0931
Mark Bates <mark@markbates.com>
2016-12-10 15:50:14 -0500

    added some godoc for Resource

--- resource.go
+++ resource.go
@@ -2,6 +2,9 @@ package buffalo
 
 import "errors"
 
+// Resource interface allows for the easy mapping
+// of common RESTful actions to a set of paths. See
+// the a.Resource documentation for more details.
 type Resource interface {
 	List(Context) error
 	Show(Context) error
@@ -29,30 +32,37 @@ type Resource interface {
 */
 type BaseResource struct{}
 
+// List default implementation. Returns a 404
 func (v *BaseResource) List(c Context) error {
 	return c.Error(404, errors.New("resource not implemented"))
 }
 
+// Show default implementation. Returns a 404
 func (v *BaseResource) Show(c Context) error {
 	return c.Error(404, errors.New("resource not implemented"))
 }
 
+// New default implementation. Returns a 404
 func (v *BaseResource) New(c Context) error {
 	return c.Error(404, errors.New("resource not implemented"))
 }
 
+// Create default implementation. Returns a 404
 func (v *BaseResource) Create(c Context) error {
 	return c.Error(404, errors.New("resource not implemented"))
 }
 
+// Edit default implementation. Returns a 404
 func (v *BaseResource) Edit(c Context) error {
 	return c.Error(404, errors.New("resource not implemented"))
 }
 
+// Update default implementation. Returns a 404
 func (v *BaseResource) Update(c Context) error {
 	return c.Error(404, errors.New("resource not implemented"))
 }
 
+// Destroy default implementation. Returns a 404
 func (v *BaseResource) Destroy(c Context) error {
 	return c.Error(404, errors.New("resource not implemented"))
 }
