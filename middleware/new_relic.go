2943b0e142e43cf409064462e2027ef2f34e8f28
Mark Bates <mark@markbates.com>
2016-12-13 22:29:52 -0500

    reworked NewRelic middleware

--- middleware/new_relic.go
+++ middleware/new_relic.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/markbates/buffalo"
 	newrelic "github.com/newrelic/go-agent"
-	"github.com/pkg/errors"
 )
 
 // NewRelic returns a piece of buffalo.Middleware that can
@@ -15,17 +14,21 @@ import (
 // is skipped and the chain continues on like normal. Useful
 // for development.
 func NewRelic(key, name string) buffalo.MiddlewareFunc {
+	mf := func(next buffalo.Handler) buffalo.Handler {
+		return next
+	}
+	if key == "" {
+		return mf
+	}
+	fmt.Printf("Setting up New Relic %s\n", key)
+	config := newrelic.NewConfig(name, key)
+	app, err := newrelic.NewApplication(config)
+	if err != nil {
+		return mf
+	}
+
 	return func(next buffalo.Handler) buffalo.Handler {
-		if key == "" {
-			return next
-		}
 		return func(c buffalo.Context) error {
-			fmt.Printf("Setting up New Relic %s\n", key)
-			config := newrelic.NewConfig(name, key)
-			app, err := newrelic.NewApplication(config)
-			if err != nil {
-				return errors.WithStack(err)
-			}
 			tx := app.StartTransaction(c.Request().URL.String(), c.Response(), c.Request())
 			defer tx.End()
 			return next(c)
