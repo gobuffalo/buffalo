1d1b1d763d3945fd77ab6ce859765b0d5bcb753f
Brian Downs <brian.downs@gmail.com>
2016-12-31 08:41:30 -0700

    issue-35: markdown clean up

--- buffalo/cmd/app_generators.go
+++ buffalo/cmd/app_generators.go
@@ -51,58 +51,57 @@ func appGoGet() *exec.Cmd {
 const nREADME = `# {{name}}
 
 ## Documentation
-
-To view generated docs for {{name}}, run the below command and point your brower to http://127.0.0.1:6060
-
- godoc -http=:6060 2>/dev/null &
-
+ 
+To view generated docs for {{name}}, run the below command and point your brower to http://127.0.0.1:6060/pkg/
+ 
+    godoc -http=:6060 2>/dev/null &
+ 
 ### Buffalo 
-
+ 
 http://gobuffalo.io/docs/getting-started
-
+ 
 ### Pop/Soda
-
+ 
 http://gobuffalo.io/docs/db
-
+ 
 ## Database Configuration
-
-	development:
-		dialect: postgres
-		database: {{name}}_development
-		user: <username>
-		password: <password>
-		host: 127.0.0.1
-		pool: 5
-
-	test:
-		dialect: postgres
-		database: {{name}}_test
-		user: <username>
-		password: <password>
-		host: 127.0.0.1
-	
-	production:
-		dialect: postgres
-		database: {{name}}_production
-		user: <username>
-		password: <password>
-		host: 127.0.0.1
-		pool: 25
-
-### Running Migrations
-
- buffalo soda migrate
-
-## Run Tests
-
- buffalo test
-
-## Run in dev
-
- buffalo dev
-
-![alt](http://www.gobuffalo.io/assets/images/logo.svg)
-[Buffalo](http://gobuffalo.io) Powered
+ 
+ 	development:
+ 		dialect: postgres
+ 		database: {{name}}_development
+ 		user: <username>
+ 		password: <password>
+ 		host: 127.0.0.1
+ 		pool: 5
+ 
+ 	test:
+ 		dialect: postgres
+ 		database: {{name}}_test
+ 		user: <username>
+ 		password: <password>
+ 		host: 127.0.0.1
+ 	
+ 	production:
+ 		dialect: postgres
+ 		database: {{name}}_production
+ 		user: <username>
+ 		password: <password>
+ 		host: 127.0.0.1
+ 		pool: 25
+ 
+ ### Running Migrations
+ 
+    buffalo soda migrate
+ 
+ ## Run Tests
+ 
+    buffalo test
+ 
+ ## Run in dev
+ 
+    buffalo dev
+ 
+[Powered by Buffalo](http://gobuffalo.io)
 
 `
 
