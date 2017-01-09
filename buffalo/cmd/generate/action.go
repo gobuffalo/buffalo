e46391f6d59941dfda349ef8c383f6627f954b9b
Antonio Pagano <apagano@wawand.co>
2017-01-09 15:22:51 -0500

    [cleaning] removing docs and implementing actions to render html

--- buffalo/cmd/generate/action.go
+++ buffalo/cmd/generate/action.go
@@ -1,17 +1,3 @@
-// Copyright © 2016 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package generate
 
 import (
@@ -95,8 +81,10 @@ func (aa actionAppender) Run(rootPath string, data gentronics.Data) error {
 	}
 
 	templateData := map[string]string{
-		"namespace": inflect.Camelize(aa.Namespace),
-		"action":    inflect.Camelize(aa.ActionName),
+		"namespace":       inflect.Camelize(aa.Namespace),
+		"action":          inflect.Camelize(aa.ActionName),
+		"namespace_under": inflect.Underscore(aa.Namespace),
+		"action_under":    inflect.Underscore(aa.ActionName),
 	}
 
 	t, _ := raymond.Parse(rActionFuncT)
@@ -126,7 +114,7 @@ const (
     
     // {{namespace}}{{action}} default implementation.
     func {{namespace}}{{action}}(c buffalo.Context) error {
-	    return c.Render(200, r.String("OK"))
+	    return r.HTML("{{namespace_under}}/{{action_under}}.html")
     }
     `
 
