a786f2b5f076fb2e4aec591a13bf1ea079f0fbf2
lumost <jcmmars@gmail.com>
2017-01-15 12:05:45 -0500

    unpack error for all middlewares

--- middleware/pop_transaction.go
+++ middleware/pop_transaction.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/gobuffalo/buffalo"
 	"github.com/markbates/pop"
-	"github.com/pkg/errors"
 )
 
 // PopTransaction is a piece of Buffalo middleware that wraps each
@@ -18,8 +17,7 @@ var PopTransaction = func(db *pop.Connection) buffalo.MiddlewareFunc {
 		return func(c buffalo.Context) error {
 			// wrap all requests in a transaction and set the length
 			// of time doing things in the db to the log.
-
-			err := db.Transaction(func(tx *pop.Connection) error {
+			return db.Transaction(func(tx *pop.Connection) error {
 				start := tx.Elapsed
 				defer func() {
 					finished := tx.Elapsed
@@ -29,14 +27,6 @@ var PopTransaction = func(db *pop.Connection) buffalo.MiddlewareFunc {
 				c.Set("tx", tx)
 				return h(c)
 			})
-			// find out if there is an underlying http error and return it rather than returning
-			// the wrapped transaction error
-			switch rootCause := errors.Cause(err).(type) {
-			case buffalo.HTTPError:
-				return rootCause
-			default:
-				return err
-			}
 		}
 	}
 }
