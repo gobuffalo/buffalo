8de2995f8740b202f85ff483c130d6c15686cf7f
Antonio Pagano <apagano@wawand.co>
2017-01-22 11:59:15 -0500

    adding some tests for the rendering of the Errors and All

--- flash_test.go
+++ flash_test.go
@@ -1,8 +1,12 @@
 package buffalo
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
+	"github.com/gobuffalo/buffalo/render"
+	"github.com/markbates/willie"
 	"github.com/stretchr/testify/require"
 )
 
@@ -71,3 +75,81 @@ func Test_FlashAdd(t *testing.T) {
 		"error": []string{"something", "other"},
 	})
 }
+
+func Test_FlashRender(t *testing.T) {
+	tempFolder := os.TempDir()
+	ioutil.WriteFile(tempFolder+"application.html", []byte("{{yield}}"), 0755)
+	ioutil.WriteFile(tempFolder+"show.html", []byte(errorsTPL), 0755)
+
+	r := require.New(t)
+	a := Automatic(Options{})
+	rr := render.New(render.Options{
+		HTMLLayout: tempFolder + "application.html",
+	})
+
+	a.GET("/", func(c Context) error {
+		c.Flash().Add("errors", "Error AJ set")
+		c.Flash().Add("errors", "Error DAL set")
+
+		return c.Render(201, rr.HTML(tempFolder+"show.html"))
+	})
+
+	w := willie.New(a)
+	res := w.Request("/").Get()
+
+	r.Contains(res.Body.String(), "Error AJ set")
+	r.Contains(res.Body.String(), "Error DAL set")
+}
+
+func Test_FlashRenderElse(t *testing.T) {
+	tempFolder := os.TempDir()
+	ioutil.WriteFile(tempFolder+"application.html", []byte("{{yield}}"), 0755)
+	ioutil.WriteFile(tempFolder+"show.html", []byte(errorsTPL), 0755)
+
+	r := require.New(t)
+	a := Automatic(Options{})
+	rr := render.New(render.Options{
+		HTMLLayout: tempFolder + "application.html",
+	})
+
+	a.GET("/", func(c Context) error {
+		return c.Render(201, rr.HTML(tempFolder+"show.html"))
+	})
+
+	w := willie.New(a)
+
+	res := w.Request("/").Get()
+	r.NotContains(res.Body.String(), "Flash:")
+}
+
+const errorsTPL = `{{#each flash.Errors as |k flash|}}
+	Flash:
+    {{k}}:{{flash}}
+{{/each}}`
+
+func Test_FlashRenderAll(t *testing.T) {
+	tempFolder := os.TempDir()
+	ioutil.WriteFile(tempFolder+"application.html", []byte("{{yield}}"), 0755)
+	ioutil.WriteFile(tempFolder+"show.html", []byte(keyTPL), 0755)
+
+	r := require.New(t)
+	a := Automatic(Options{})
+	rr := render.New(render.Options{
+		HTMLLayout: tempFolder + "application.html",
+	})
+
+	a.GET("/", func(c Context) error {
+		c.Flash().Set("something", "something to say!")
+		return c.Render(201, rr.HTML(tempFolder+"show.html"))
+	})
+
+	w := willie.New(a)
+	res := w.Request("/").Get()
+
+	r.Contains(res.Body.String(), "something to say!")
+}
+
+const keyTPL = `{{#each flash.All as |k flash|}}
+	Flash:
+    {{k}}:{{flash}}
+{{/each}}`
