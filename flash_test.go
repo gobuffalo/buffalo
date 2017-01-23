442fbf452ddb3f2b9befe101af6ef6c4d164e1f7
Antonio Pagano <apagano@wawand.co>
2017-01-23 13:56:48 -0500

    [testing] adding cases for custom keys

--- flash_test.go
+++ flash_test.go
@@ -103,3 +103,49 @@ const keyTPL = `{{#each flash as |k flash|}}
 	Flash:
     {{k}}:{{flash}}
 {{/each}}`
+
+func Test_FlashRenderCustomKey(t *testing.T) {
+	tempFolder := os.TempDir()
+	ioutil.WriteFile(tempFolder+"application.html", []byte("{{yield}}"), 0755)
+	ioutil.WriteFile(tempFolder+"show.html", []byte(customKeyTPL), 0755)
+
+	r := require.New(t)
+	a := Automatic(Options{})
+	rr := render.New(render.Options{
+		HTMLLayout: tempFolder + "application.html",
+	})
+
+	a.GET("/", func(c Context) error {
+		c.Flash().Add("something", "something to say!")
+		return c.Render(201, rr.HTML(tempFolder+"show.html"))
+	})
+
+	w := willie.New(a)
+	res := w.Request("/").Get()
+	r.Contains(res.Body.String(), "something to say!")
+}
+
+func Test_FlashRenderCustomKeyNotDefined(t *testing.T) {
+	tempFolder := os.TempDir()
+	ioutil.WriteFile(tempFolder+"application.html", []byte("{{yield}}"), 0755)
+	ioutil.WriteFile(tempFolder+"show.html", []byte(customKeyTPL), 0755)
+
+	r := require.New(t)
+	a := Automatic(Options{})
+	rr := render.New(render.Options{
+		HTMLLayout: tempFolder + "application.html",
+	})
+
+	a.GET("/", func(c Context) error {
+		c.Flash().Add("other", "something to say!")
+		return c.Render(201, rr.HTML(tempFolder+"show.html"))
+	})
+
+	w := willie.New(a)
+	res := w.Request("/").Get()
+	r.NotContains(res.Body.String(), "something to say!")
+}
+
+const customKeyTPL = `{{#each flash.something as |k flash|}}
+	{{flash}}
+{{/each}}`
