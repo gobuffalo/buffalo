ef8012a8271bae25f177cdd22d0b83870850d954
Antonio Pagano <apagano@wawand.co>
2017-01-31 08:23:24 -0500

    making code inside app.go not be pointer specific and making tests pass

--- buffalo/cmd/generate/resource_test.go
+++ buffalo/cmd/generate/resource_test.go
@@ -34,13 +34,13 @@ func TestGenerateResourceCode(t *testing.T) {
 
 	fileData, _ := ioutil.ReadFile("actions/app.go")
 	r.Contains(string(fileData), "var usersResource buffalo.Resource")
-	r.Contains(string(fileData), "usersResource = &UsersResource{&buffalo.BaseResource{}}")
+	r.Contains(string(fileData), "usersResource = UsersResource{&buffalo.BaseResource{}}")
 	r.Contains(string(fileData), "app.Resource(\"/users\", usersResource)")
 
 	fileData, _ = ioutil.ReadFile("actions/users.go")
 	r.Contains(string(fileData), "type UsersResource struct {")
-	r.Contains(string(fileData), "func (v *UsersResource) List(c buffalo.Context) error {")
-	r.Contains(string(fileData), "func (v *UsersResource) Destroy(c buffalo.Context) error {")
+	r.Contains(string(fileData), "func (v UsersResource) List(c buffalo.Context) error {")
+	r.Contains(string(fileData), "func (v UsersResource) Destroy(c buffalo.Context) error {")
 
 	fileData, _ = ioutil.ReadFile("actions/users_test.go")
 	r.Contains(string(fileData), "func Test_UsersResource_List")
