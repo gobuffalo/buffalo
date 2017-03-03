a9dda85781df7f8e810416a21e6e9da355ca2434
Mark Bates <mark@markbates.com>
2017-03-03 18:15:16 -0500

    switched to using actual folders for generator templates instead of splitting on "-"

--- generators/common/files.go
+++ generators/common/files.go
@@ -19,15 +19,25 @@ type Files []File
 
 // Find all the .tmpl files inside the buffalo GOPATH
 func Find(path string) (Files, error) {
-	root := filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "gobuffalo", "buffalo", "generators", path)
+	root := filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "gobuffalo", "buffalo", "generators", path, "templates")
 	files := Files{}
 	err := filepath.Walk(root, func(p string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			if filepath.Ext(p) == ".tmpl" {
 				f := File{ReadPath: p}
-				base := filepath.Base(p)
+				rel := strings.TrimPrefix(p, root)
+
+				paths := strings.Split(rel, string(os.PathSeparator))
+
+				li := len(paths) - 1
+				base := paths[li]
 				base = strings.TrimSuffix(base, ".tmpl")
-				f.WritePath = filepath.Join(strings.Split(base, "-")...)
+				if strings.HasPrefix(base, "dot-") {
+					base = "." + strings.TrimPrefix(base, "dot-")
+				}
+				paths[li] = base
+				f.WritePath = filepath.Join(paths...)
+
 				b, err := ioutil.ReadFile(p)
 				if err != nil {
 					return err
--- generators/refresh/refresh.go
+++ generators/refresh/refresh.go
@@ -1,37 +1,22 @@
 package refresh
 
-import "github.com/markbates/gentronics"
+import (
+	"github.com/gobuffalo/buffalo/generators/common"
+	"github.com/markbates/gentronics"
+)
 
 // New generator for a .buffalo.dev.yml file
-func New() *gentronics.Generator {
+func New() (*gentronics.Generator, error) {
 	g := gentronics.New()
 
-	g.Add(gentronics.NewFile(".buffalo.dev.yml", nRefresh))
-	return g
-}
+	files, err := common.Find("refresh")
+	if err != nil {
+		return nil, err
+	}
 
-const nRefresh = `app_root: .
-ignored_folders:
-- vendor
-- log
-- logs
-- assets
-- public
-- grifts
-- tmp
-- bin
-- node_modules
-- .sass-cache
-included_extensions:
-- .go
-- .html
-- .md
-- .js
-- .tmpl
-build_path: tmp
-build_delay: 200ns
-binary_name: {{name}}-build
-command_flags: []
-enable_colors: true
-log_name: buffalo
-`
+	for _, f := range files {
+		g.Add(gentronics.NewFile(f.WritePath, f.Body))
+	}
+
+	return g, nil
+}
