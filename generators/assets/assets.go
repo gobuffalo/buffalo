22631201cb3017cc96159a5e37c2874a47341ecf
Mark Bates <mark@markbates.com>
2017-03-03 18:55:27 -0500

    cleaned up the asset generators a bit

--- generators/assets/assets.go
+++ generators/assets/assets.go
@@ -1,15 +1,4 @@
 package assets
 
-import "github.com/markbates/gentronics"
-
+// LogoURL is the URL to the SVG logo to be used for assets
 const LogoURL = "https://raw.githubusercontent.com/gobuffalo/buffalo/master/logo.svg"
-
-var PublicLogo = &gentronics.RemoteFile{
-	File:       gentronics.NewFile("public/assets/images/logo.svg", ""),
-	RemotePath: LogoURL,
-}
-
-var AssetsLogo = &gentronics.RemoteFile{
-	File:       gentronics.NewFile("assets/images/logo.svg", ""),
-	RemotePath: LogoURL,
-}
