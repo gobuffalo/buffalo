61156ca117dfbd10ca492372effcf33eda761724
Mark Bates <mark@markbates.com>
2017-03-05 15:39:58 -0500

    converted the resource generator

--- buffalo/cmd/generate/resource.go
+++ buffalo/cmd/generate/resource.go
@@ -2,10 +2,8 @@ package generate
 
 import (
 	"errors"
-	"fmt"
-	"path/filepath"
 
-	"github.com/gobuffalo/buffalo/generators"
+	"github.com/gobuffalo/buffalo/generators/resource"
 	"github.com/markbates/gentronics"
 	"github.com/markbates/inflect"
 	"github.com/spf13/cobra"
@@ -30,56 +28,10 @@ var ResourceCmd = &cobra.Command{
 			"downFirstCap": inflect.CamelizeDownFirst(name),
 			"actions":      []string{"List", "Show", "New", "Create", "Edit", "Update", "Destroy"},
 		}
-		return NewResourceGenerator(data).Run(".", data)
+		g, err := resource.New(data)
+		if err != nil {
+			return err
+		}
+		return g.Run(".", data)
 	},
 }
-
-// NewResourceGenerator generates a new actions/resource file and a stub test.
-func NewResourceGenerator(data gentronics.Data) *gentronics.Generator {
-	g := gentronics.New()
-	g.Add(gentronics.NewFile(filepath.Join("actions", fmt.Sprintf("%s.go", data["downFirstCap"])), rAction))
-	g.Add(gentronics.NewFile(filepath.Join("actions", fmt.Sprintf("%s_test.go", data["under"])), rResourceTest))
-	g.Add(&gentronics.Func{
-		Should: func(data gentronics.Data) bool { return true },
-		Runner: func(root string, data gentronics.Data) error {
-			return generators.AddInsideAppBlock(fmt.Sprintf("var %sResource buffalo.Resource", data["downFirstCap"]),
-				fmt.Sprintf("%sResource = %sResource{&buffalo.BaseResource{}}", data["downFirstCap"], data["camel"]),
-				fmt.Sprintf("app.Resource(\"/%s\", %sResource)", data["under"], data["downFirstCap"]),
-			)
-		},
-	})
-	g.Add(gentronics.NewCommand(generators.GoFmt()))
-	return g
-}
-
-var rAction = `package actions
-
-import "github.com/gobuffalo/buffalo"
-
-type {{camel}}Resource struct{
-	buffalo.Resource
-}
-
-{{#each actions}}
-// {{.}} default implementation.
-func (v {{camel}}Resource) {{.}}(c buffalo.Context) error {
-	return c.Render(200, r.String("{{camel}}#{{.}}"))
-}
-
-{{/each}}
-`
-
-var rResourceTest = `package actions_test
-
-import (
-	"testing"
-
-	"github.com/stretchr/testify/require"
-)
-{{#each actions}}
-func Test_{{camel}}Resource_{{camelize .}}(t *testing.T) {
-	r := require.New(t)
-	r.Fail("Not Implemented!")
-}
-{{/each}}
-`
---
+++ generators/resource/resource.go
@@ -0,0 +1,32 @@
+package resource
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gobuffalo/buffalo/generators"
+	"github.com/markbates/gentronics"
+)
+
+// Newgenerates a new actions/resource file and a stub test.
+func New(data gentronics.Data) (*gentronics.Generator, error) {
+	g := gentronics.New()
+	files, err := generators.Find("resource")
+	if err != nil {
+		return nil, err
+	}
+	for _, f := range files {
+		g.Add(gentronics.NewFile(strings.Replace(f.WritePath, "resource-name", data["under"].(string), -1), f.Body))
+	}
+	g.Add(&gentronics.Func{
+		Should: func(data gentronics.Data) bool { return true },
+		Runner: func(root string, data gentronics.Data) error {
+			return generators.AddInsideAppBlock(fmt.Sprintf("var %sResource buffalo.Resource", data["downFirstCap"]),
+				fmt.Sprintf("%sResource = %sResource{&buffalo.BaseResource{}}", data["downFirstCap"], data["camel"]),
+				fmt.Sprintf("app.Resource(\"/%s\", %sResource)", data["under"], data["downFirstCap"]),
+			)
+		},
+	})
+	g.Add(gentronics.NewCommand(generators.GoFmt()))
+	return g, nil
+}
