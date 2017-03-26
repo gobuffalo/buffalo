39cfb1da8d47bead8864b96032cf7d5e3da10d3b
Spencer Dixon <spencercdixon@gmail.com>
2017-03-26 10:17:31 -0400

    Adds a buffalo version command to the CLI

--- buffalo/cmd/version.go
+++ buffalo/cmd/version.go
@@ -1,4 +1,23 @@
 package cmd
 
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
 // Version is the current version of the buffalo binary
 const Version = "0.8.0.dev"
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of buffalo",
+	Long:  `All software has versions.  This is buffalo's.`,
+	Run: func(c *cobra.Command, args []string) {
+		fmt.Println("Buffalo version %s", Version)
+	},
+}
