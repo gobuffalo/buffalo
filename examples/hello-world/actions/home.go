c24cfea9c44cccf66b3b9a567cbfce69a02e473e
Mark Bates <mark@markbates.com>
2017-05-04 10:10:48 -0400

    Remove or rebuild the apps in the examples folder #376

--- examples/hello-world/actions/home.go
+++
@@ -1,8 +0,0 @@
-package actions
-
-import "github.com/gobuffalo/buffalo"
-
-// HomeHandler renders index.html
-func HomeHandler(c buffalo.Context) error {
-	return c.Render(200, r.HTML("index.html"))
-}
