4f43891395eb7c340e8659c8ce0c12643b49ce47
Mark Bates <mark@markbates.com>
2017-05-04 10:15:15 -0400

    Remove deprecated functions slated for 0.9.0 #418

--- default_context.go
+++ default_context.go
@@ -7,8 +7,6 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"runtime"
-	"strconv"
 	"strings"
 	"time"
 
@@ -62,40 +60,10 @@ func (d *DefaultContext) Param(key string) string {
 	return d.Params().Get(key)
 }
 
-// ParamInt tries to convert the requested parameter to
-// an int. It will return an error if there is a problem.
-func (d *DefaultContext) ParamInt(key string) (int, error) {
-	warningMsg := "Context#ParamInt is deprecated, and will be removed in v0.9.0."
-
-	_, file, no, ok := runtime.Caller(1)
-	if ok {
-		warningMsg = fmt.Sprintf("%s Called from %s:%d", warningMsg, file, no)
-	}
-
-	d.Logger().Warn(warningMsg)
-
-	k := d.Params().Get(key)
-	i, err := strconv.Atoi(k)
-	return i, errors.WithMessage(err, fmt.Sprintf("could not convert %s to an int", k))
-}
-
 // Set a value onto the Context. Any value set onto the Context
 // will be automatically available in templates.
 func (d *DefaultContext) Set(key string, value interface{}) {
 	d.data[key] = value
-}
-
-// Get is deprecated. Please use Value instead.
-func (d *DefaultContext) Get(key string) interface{} {
-	warningMsg := "Context#Get is deprecated, and will be removed in v0.9.0. Please use Context#Value instead."
-
-	_, file, no, ok := runtime.Caller(1)
-	if ok {
-		warningMsg = fmt.Sprintf("%s Called from %s:%d", warningMsg, file, no)
-	}
-
-	d.Logger().Warn(warningMsg)
-	return d.Value(key)
 }
 
 // Value that has previously stored on the context.
