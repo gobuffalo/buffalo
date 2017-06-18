979672b5be36b7d708f7b21ecfceb362558e1bd5
Antonio Pagano <apagano@wawand.co>
2017-06-18 10:35:38 -0500

    updating templates and router to work correctly

--- router.go
+++ router.go
@@ -82,8 +82,11 @@ func (a *App) ServeFiles(p string, root http.FileSystem) {
 	g.DELETE("/{user_id}", ur.Destroy) DELETE /users/{user_id} => ur.Destroy
 */
 func (a *App) Resource(p string, r Resource) *App {
-	base := path.Base(p)
-	single := inflect.Singularize(base)
+	paramName := inflect.Singularize(strings.Replace(p, "/", "_", -1))
+	if strings.HasPrefix(paramName, "_") {
+		paramName = paramName[1:len(paramName)]
+	}
+
 	g := a.Group(p)
 	p = "/"
 
@@ -94,17 +97,15 @@ func (a *App) Resource(p string, r Resource) *App {
 	rt := rv.Type()
 	rname := fmt.Sprintf("%s.%s", rt.PkgPath(), rt.Name()) + ".%s"
 
-	spath := path.Join(p, fmt.Sprintf("{%s_id}", single))
+	spath := path.Join(p, fmt.Sprintf("{%s_id}", paramName))
 	setFuncKey(r.List, fmt.Sprintf(rname, "List"))
 	g.GET(p, r.List)
 	setFuncKey(r.New, fmt.Sprintf(rname, "New"))
 	g.GET(path.Join(p, "new"), r.New)
-	//.Name(inflect.Camelize(fmt.Sprintf("new_" + single)))
 	setFuncKey(r.Show, fmt.Sprintf(rname, "Show"))
 	g.GET(path.Join(spath), r.Show)
 	setFuncKey(r.Edit, fmt.Sprintf(rname, "Edit"))
 	g.GET(path.Join(spath, "edit"), r.Edit)
-	//.Name(inflect.Camelize(fmt.Sprintf("edit_" + single)))
 	setFuncKey(r.Create, fmt.Sprintf(rname, "Create"))
 	g.POST(p, r.Create)
 	setFuncKey(r.Update, fmt.Sprintf(rname, "Update"))
