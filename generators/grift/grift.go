c132f2d88e39ec1953dd5b6aba7c7138e4a7af36
Antonio Pagano <apagano@wawand.co>
2017-06-23 17:07:32 -0500

    appending to the existing grift in case file already exist

--- generators/grift/grift.go
+++ generators/grift/grift.go
@@ -1,6 +1,8 @@
 package grift
 
 import (
+	"io/ioutil"
+	"os"
 	"path/filepath"
 
 	"github.com/gobuffalo/buffalo/generators"
@@ -18,7 +20,49 @@ func New(data makr.Data) (*makr.Generator, error) {
 
 	path := filepath.Join("grifts", data["filename"].(string))
 	file := files[0]
-	g.Add(makr.NewFile(path, file.Body))
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		g.Add(makr.NewFile(path, file.Body))
+	} else {
+		template, err := ioutil.ReadFile(path)
+		if err != nil {
+			return nil, err
+		}
+
+		g.Add(makr.NewFile(path, string(template)+existsTmpl))
+	}
 
 	return g, nil
 }
+
+var existsTmpl = `
+{{ if .plainTask -}}
+    var _ = Desc("{{.taskName}}", "TODO")
+    var _ = Add("{{.taskName}}", func(c *Context) error {
+        return nil
+    })
+{{ else }}  
+    {{ $last := .last }}
+    {{ range $index, $element := .parts }}
+        {{ $isLast := eq $index $last }}
+        {{ if not $isLast }}
+            {{if eq $index 0}}
+                var _ = Desc("{{$element}}", "TODO")
+                var _ = Namespace("{{$element}}", func(){
+            {{ else }}
+                Desc("{{$element}}", "TODO")
+                Namespace("{{$element}}", func(){
+            {{end}}
+        {{ else }}
+            Desc("{{$element}}", "TODO")
+            Add("{{$element}}", func(c *Context) error{
+                return nil
+            })
+        {{ end }}
+    {{ end }}
+
+    {{ range $index, $element := .parts }}
+        {{ if $index }} }) {{ end }}
+    {{ end }}
+               
+{{ end }}`
