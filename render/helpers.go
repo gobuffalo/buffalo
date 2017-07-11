39be7191d3bfb69e2af031227747359e483723a3
Mark Bates <mark@markbates.com>
2017-07-11 12:11:33 -0400

    add pagination helper to generated resources

---
+++ render/helpers.go
@@ -0,0 +1,24 @@
+package render
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/gobuffalo/plush"
+	"github.com/gobuffalo/tags"
+	"github.com/markbates/pop"
+	"github.com/pkg/errors"
+)
+
+func init() {
+	plush.Helpers.Add("paginator", func(pagination *pop.Paginator, opts map[string]interface{}, help plush.HelperContext) (template.HTML, error) {
+		if opts["path"] == nil {
+			opts["path"] = help.Value("request").(*http.Request).URL.String()
+		}
+		t, err := tags.Pagination(pagination, opts)
+		if err != nil {
+			return "", errors.WithStack(err)
+		}
+		return t.HTML(), nil
+	})
+}
