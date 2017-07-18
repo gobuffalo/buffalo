f957b71472b9648595b1acc21b9c2057e269e834
Mark Bates <mark@markbates.com>
2017-07-18 10:58:45 -0400

    added an `info` command to print of env/versions to help with debugging

---
+++ buffalo/cmd/info.go
@@ -0,0 +1,102 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+// infoCmd represents the info command
+var infoCmd = &cobra.Command{
+	Use:   "info",
+	Short: "Prints off diagnostic information useful for debugging.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		bb := os.Stdout
+
+		bb.WriteString(fmt.Sprintf("### Buffalo Version\n%s\n", Version))
+
+		bb.WriteString("\n### Go Version\n")
+		c := exec.Command("go", "version")
+		c.Stdout = bb
+		err := c.Run()
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		bb.WriteString("\n### Go Env\n")
+		c = exec.Command("go", "env")
+		c.Stdout = bb
+		c.Stderr = bb
+		c.Run()
+
+		bb.WriteString("\n### Node Version\n")
+		if _, err := exec.LookPath("node"); err == nil {
+			c = exec.Command("node", "--version")
+			c.Stdout = bb
+			c.Stderr = bb
+			c.Run()
+		} else {
+			bb.WriteString("Node Not Found\n")
+		}
+
+		bb.WriteString("\n### NPM Version\n")
+		if _, err := exec.LookPath("npm"); err == nil {
+			c = exec.Command("npm", "--version")
+			c.Stdout = bb
+			c.Stderr = bb
+			c.Run()
+		} else {
+			bb.WriteString("NPM Not Found\n")
+		}
+
+		bb.WriteString("\n### Dep Status\n")
+		if _, err := exec.LookPath("dep"); err == nil {
+			c = exec.Command("dep", "status")
+			c.Stdout = bb
+			c.Stderr = bb
+			c.Run()
+		} else {
+			bb.WriteString("dep Not Found\n")
+		}
+
+		bb.WriteString("\n### PostgreSQL Version\n")
+		if _, err := exec.LookPath("pg_ctl"); err == nil {
+			c = exec.Command("pg_ctl", "--version")
+			c.Stdout = bb
+			c.Stderr = bb
+			c.Run()
+		} else {
+			bb.WriteString("PostgreSQL Not Found\n")
+		}
+
+		bb.WriteString("\n### MySQL Version\n")
+		if _, err := exec.LookPath("mysql"); err == nil {
+			c = exec.Command("mysql", "--version")
+			c.Stdout = bb
+			c.Stderr = bb
+			c.Run()
+		} else {
+			bb.WriteString("MySQL Not Found\n")
+		}
+
+		bb.WriteString("\n### SQLite Version\n")
+		if _, err := exec.LookPath("sqlite3"); err == nil {
+			c = exec.Command("sqlite3", "--version")
+			c.Stdout = bb
+			c.Stderr = bb
+			c.Run()
+		} else {
+			bb.WriteString("MySQL Not Found\n")
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	decorate("info", RootCmd)
+	RootCmd.AddCommand(infoCmd)
+}
