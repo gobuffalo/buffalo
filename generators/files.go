956dd5460725d716b37435fec211a13258e4484f
Mark Bates <mark@markbates.com>
2017-08-22 16:43:40 -0400

    removed a bit of unneeded code

--- generators/files.go
+++ generators/files.go
@@ -1,7 +1,6 @@
 package generators
 
 import (
-	"errors"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -25,13 +24,9 @@ type Files []File
 
 // Find all the .tmpl files inside the buffalo GOPATH
 func Find(path string) (Files, error) {
-	gp, err := goPath()
-	if err != nil {
-		return nil, err
-	}
-	root := filepath.Join(gp, "src", path, "templates")
+	root := filepath.Join(envy.GoPath(), "src", path, "templates")
 	files := Files{}
-	err = filepath.Walk(root, func(p string, info os.FileInfo, err error) error {
+	err := filepath.Walk(root, func(p string, info os.FileInfo, err error) error {
 		if info != nil && !info.IsDir() {
 			if filepath.Ext(p) == ".tmpl" {
 				f := File{ReadPath: p}
@@ -59,19 +54,4 @@ func Find(path string) (Files, error) {
 		return nil
 	})
 	return files, err
-}
-
-func goPath() (string, error) {
-	for _, path := range envy.GoPaths() {
-		pp := filepath.Join(path, "src", "github.com", "gobuffalo", "buffalo")
-		if exists(pp) {
-			return path, nil
-		}
-	}
-	return "", errors.New("buffalo was not found")
-}
-
-func exists(path string) bool {
-	_, err := os.Stat(path)
-	return err == nil
 }
