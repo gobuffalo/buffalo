851921f99ad8ed2c254b812b21ef58f091517567
Mark Bates <mark@markbates.com>
2017-10-10 17:29:04 -0400

    fixed broken tests

--- buffalo/cmd/plugins.go
+++ buffalo/cmd/plugins.go
@@ -31,32 +31,34 @@ func plugs() plugins.List {
 func decorate(name string, cmd *cobra.Command) {
 	pugs := plugs()
 	for _, c := range pugs[name] {
-		anywhereCommands = append(anywhereCommands, c.Name)
-		cc := &cobra.Command{
-			Use:     c.Name,
-			Short:   fmt.Sprintf("[PLUGIN] %s", c.Description),
-			Aliases: c.Aliases,
-			RunE: func(cmd *cobra.Command, args []string) error {
-				plugCmd := c.Name
-				if c.UseCommand != "" {
-					plugCmd = c.UseCommand
-				}
+		func(c plugins.Command) {
+			anywhereCommands = append(anywhereCommands, c.Name)
+			cc := &cobra.Command{
+				Use:     c.Name,
+				Short:   fmt.Sprintf("[PLUGIN] %s", c.Description),
+				Aliases: c.Aliases,
+				RunE: func(cmd *cobra.Command, args []string) error {
+					plugCmd := c.Name
+					if c.UseCommand != "" {
+						plugCmd = c.UseCommand
+					}
 
-				ax := []string{plugCmd}
-				if plugCmd == "-" {
-					ax = []string{}
-				}
+					ax := []string{plugCmd}
+					if plugCmd == "-" {
+						ax = []string{}
+					}
 
-				ax = append(ax, args...)
-				ex := exec.Command(c.Binary, ax...)
-				ex.Env = append(os.Environ(), "BUFFALO_PLUGIN=1")
-				ex.Stdin = os.Stdin
-				ex.Stdout = os.Stdout
-				ex.Stderr = os.Stderr
-				return ex.Run()
-			},
-		}
-		cc.DisableFlagParsing = true
-		cmd.AddCommand(cc)
+					ax = append(ax, args...)
+					ex := exec.Command(c.Binary, ax...)
+					ex.Env = append(os.Environ(), "BUFFALO_PLUGIN=1")
+					ex.Stdin = os.Stdin
+					ex.Stdout = os.Stdout
+					ex.Stderr = os.Stderr
+					return ex.Run()
+				},
+			}
+			cc.DisableFlagParsing = true
+			cmd.AddCommand(cc)
+		}(c)
 	}
 }
