f3864923eeb289040a3d88bfaa46e6f3e9eb9e22
Mark Bates <mark@markbates.com>
2017-10-11 10:51:22 -0400

    removed an os.Stat from the build tool

--- buffalo/cmd/build/assets.go
+++ buffalo/cmd/build/assets.go
@@ -1,16 +1,13 @@
 package build
 
 import (
-	"os"
-	"path/filepath"
-
 	"github.com/gobuffalo/buffalo/generators/assets/webpack"
 	pack "github.com/gobuffalo/packr/builder"
 	"github.com/pkg/errors"
 )
 
 func (b *Builder) buildAssets() error {
-	if _, err := os.Stat(filepath.Join(b.Root, "webpack.config.js")); err == nil {
+	if b.WithWebpack {
 		err := b.exec(webpack.BinPath)
 		if err != nil {
 			return errors.WithStack(err)
