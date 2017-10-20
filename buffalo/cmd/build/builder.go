5846d7c096fbe1d2faff14df62c0ef7fc23c2d29
Antonio Pagano <apagano@wawand.co>
2017-10-20 14:32:14 -0500

    moving skip-assets logic to be inside the buildAssets step

--- buffalo/cmd/build/builder.go
+++ buffalo/cmd/build/builder.go
@@ -38,12 +38,6 @@ func New(ctx context.Context, opts Options) *Builder {
 		b.buildBin,
 	}
 
-	if b.Options.SkipAssets {
-		logrus.Debug("skipping assets compilation step")
-		b.steps = append(b.steps[:6], b.steps[7:]...)
-		return b
-	}
-
 	return b
 }
 
