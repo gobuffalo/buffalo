b63c0340ea3489e8bff027937d77f9f62cac6c28
Mark Bates <mark@markbates.com>
2017-10-25 18:20:14 -0400

    fixes go templates

--- render/template_engine.go
+++ render/template_engine.go
@@ -10,13 +10,21 @@ type TemplateEngine func(input string, data map[string]interface{}, helpers map[
 
 // GoTemplateEngine implements the TemplateEngine interface for using standard Go templates
 func GoTemplateEngine(input string, data map[string]interface{}, helpers map[string]interface{}) (string, error) {
-	t, err := template.New(input).Parse(input)
-	if err != nil {
-		return "", err
-	}
+	// since go templates don't have the concept of an optional map argument like Plush does
+	// add this "null" map so it can be used in templates like this:
+	// {{ partial "flash.html" .nilOpts }}
+	data["nilOpts"] = map[string]interface{}{}
+
+	t := template.New(input)
 	if helpers != nil {
 		t = t.Funcs(helpers)
 	}
+
+	t, err := t.Parse(input)
+	if err != nil {
+		return "", err
+	}
+
 	bb := &bytes.Buffer{}
 	err = t.Execute(bb, data)
 	return bb.String(), err
