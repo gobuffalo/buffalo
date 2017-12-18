119ba96e9251e032bdb1a1528f24ce128186d099
Mark Bates <mark@markbates.com>
2017-12-18 15:52:21 -0500

    cleaned up a few things

--- binding/file.go
+++ binding/file.go
@@ -13,42 +13,6 @@ import (
 // for more information on how this impacts file uploads.
 var MaxFileSize int64 = 5 * 1024 * 1024
 
-func init() {
-	sb := func(req *http.Request, i interface{}) error {
-		err := req.ParseMultipartForm(MaxFileSize)
-		if err != nil {
-			return errors.WithStack(err)
-		}
-		if err := decoder.Decode(req.Form, i); err != nil {
-			return errors.WithStack(err)
-		}
-
-		form := req.MultipartForm.File
-		if len(form) == 0 {
-			return nil
-		}
-
-		ri := reflect.Indirect(reflect.ValueOf(i))
-		for n, _ := range form {
-			f := ri.FieldByName(n)
-			if _, ok := f.Interface().(File); !ok {
-				continue
-			}
-			mf, mh, err := req.FormFile(n)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-			f.Set(reflect.ValueOf(File{
-				File:       mf,
-				FileHeader: mh,
-			}))
-		}
-
-		return nil
-	}
-	binders["multipart/form-data"] = sb
-}
-
 // File holds information regarding an uploaded file
 type File struct {
 	multipart.File
@@ -68,4 +32,40 @@ func (f File) String() string {
 		return ""
 	}
 	return f.Filename
+}
+
+func init() {
+	sb := func(req *http.Request, i interface{}) error {
+		err := req.ParseMultipartForm(MaxFileSize)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		if err := decoder.Decode(req.Form, i); err != nil {
+			return errors.WithStack(err)
+		}
+
+		form := req.MultipartForm.File
+		if len(form) == 0 {
+			return nil
+		}
+
+		ri := reflect.Indirect(reflect.ValueOf(i))
+		for n := range form {
+			f := ri.FieldByName(n)
+			if _, ok := f.Interface().(File); !ok {
+				continue
+			}
+			mf, mh, err := req.FormFile(n)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+			f.Set(reflect.ValueOf(File{
+				File:       mf,
+				FileHeader: mh,
+			}))
+		}
+
+		return nil
+	}
+	binders["multipart/form-data"] = sb
 }
