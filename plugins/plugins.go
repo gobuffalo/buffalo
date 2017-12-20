bc064918af54ad1cbb3600d39502c8f0331b333c
backyio <backystart@gmail.com>
2017-12-20 10:37:28 +0100

    [changed] Using BUFFALO_PLUGIN_PATH instead of PATH           this fix windows slow startup and full directory scan in all platform

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"runtime"
 	"strings"
 	"time"
 
@@ -27,16 +26,11 @@ type List map[string]Commands
 // * file/command must respond to `available` and return JSON of
 //	 plugins.Commands{}
 //
-// Caveats:
-// * The C:\Windows directory is excluded
+// Limit full path scan with direct plugin path
 func Available() (List, error) {
 	list := List{}
 	paths := []string{"plugins"}
-	if runtime.GOOS == "windows" {
-		paths = append(paths, strings.Split(os.Getenv("PATH"), ";")...)
-	} else {
-		paths = append(paths, strings.Split(os.Getenv("PATH"), ":")...)
-	}
+	paths = append(paths, strings.Split(os.Getenv("BUFFALO_PLUGIN_PATH"), ";")...)
 	for _, p := range paths {
 		if ignorePath(p) {
 			continue
