176c468b6e8de177e10bd696666f98aeb8f6e8da
Mark Bates <mark@markbates.com>
2018-04-26 14:22:11 -0400

    Allow buffalo to accept preconfigured http.Server(s) fixes #1015 (#1039)

---
+++ servers/servers.go
@@ -0,0 +1,37 @@
+package servers
+
+import (
+	"context"
+	"net"
+	"net/http"
+)
+
+// Server allows for custom server implementations
+type Server interface {
+	Shutdown(context.Context) error
+	Start(context.Context, http.Handler) error
+	SetAddr(string)
+}
+
+// Wrap converts a standard *http.Server to a buffalo.Server
+func Wrap(s *http.Server) Server {
+	return &Simple{Server: s}
+}
+
+// WrapTLS Server converts a standard *http.Server to a buffalo.Server
+// but makes sure it is run with TLS.
+func WrapTLS(s *http.Server, certFile string, keyFile string) Server {
+	return &TLS{
+		Server:   s,
+		CertFile: certFile,
+		KeyFile:  keyFile,
+	}
+}
+
+// WrapListener wraps an *http.Server and a net.Listener
+func WrapListener(s *http.Server, l net.Listener) Server {
+	return &Listener{
+		Server:   s,
+		Listener: l,
+	}
+}
