3076c073e443333dfe45e1c4cc67aebbaec376df
Mark Bates <mark@markbates.com>
2018-04-26 14:22:11 -0400

    Allow buffalo to accept preconfigured http.Server(s) fixes #1015 (#1039)

---
+++ servers/tls.go
@@ -0,0 +1,26 @@
+package servers
+
+import (
+	"context"
+	"net/http"
+)
+
+// TLS server
+type TLS struct {
+	*http.Server
+	CertFile string
+	KeyFile  string
+}
+
+// SetAddr sets the servers address, if it hasn't already been set
+func (s *TLS) SetAddr(addr string) {
+	if s.Server.Addr == "" {
+		s.Server.Addr = addr
+	}
+}
+
+// Start the server
+func (s *TLS) Start(c context.Context, h http.Handler) error {
+	s.Handler = h
+	return s.ListenAndServeTLS(s.CertFile, s.KeyFile)
+}
