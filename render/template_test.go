6511c32b1cf2635142705812d086c56b45eef162
Mark Bates <mark@markbates.com>
2018-05-02 14:16:27 -0400

    added a test

--- render/template_test.go
+++ render/template_test.go
@@ -78,6 +78,35 @@ func Test_Template_Partial(t *testing.T) {
 	r.Equal("Foo > Mark", strings.TrimSpace(bb.String()))
 }
 
+func Test_Template_Partial_Recursive_With_Global_And_Local_Context(t *testing.T) {
+	r := require.New(t)
+
+	tPath, err := ioutil.TempDir("", "")
+	r.NoError(err)
+	defer os.Remove(tPath)
+
+	partFile, err := os.Create(filepath.Join(tPath, "_foo.html"))
+	r.NoError(err)
+
+	_, err = partFile.Write([]byte(`<%= other %>|<%= name %>`))
+	r.NoError(err)
+
+	tmpFile, err := os.Create(filepath.Join(tPath, "index.html"))
+	r.NoError(err)
+
+	_, err = tmpFile.Write([]byte(`<%= partial("foo.html", {other: "Other"}) %>`))
+	r.NoError(err)
+
+	re := render.New(render.Options{
+		TemplatesBox: packr.NewBox(tPath),
+	}).Template("", "index.html")
+
+	bb := &bytes.Buffer{}
+	err = re.Render(bb, render.Data{"name": "Mark"})
+	r.NoError(err)
+	r.Equal("Other|Mark", strings.TrimSpace(bb.String()))
+}
+
 func Test_Template_Partial_WithoutExtension(t *testing.T) {
 	r := require.New(t)
 
