06eeb585a0ead9e6575184adb888b19786377f4e
Antonio Pagano <apagano@wawand.co>
2018-05-07 20:37:15 -0500

    adding resource fields validation

--- generators/resource/generator_test.go
+++ generators/resource/generator_test.go
@@ -14,3 +14,32 @@ func Test_New_WithNestedName(t *testing.T) {
 	name := g.Name
 	r.Equal("admin_user_id", name.ParamID())
 }
+
+func Test_New_WithPropertyNames(t *testing.T) {
+	r := require.New(t)
+
+	cases := []struct {
+		Case     string
+		Args     []string
+		HasError bool
+	}{
+		{Case: "", Args: []string{"", "body,name"}, HasError: true},
+		{Case: "", Args: []string{"", "body.name"}, HasError: true},
+		{Case: "", Args: []string{"", "body", "name"}, HasError: false},
+		{Case: "", Args: []string{"", "body-name"}, HasError: false},
+		{Case: "", Args: []string{"", "body_name"}, HasError: false},
+		{Case: "", Args: []string{"", "9A"}, HasError: true},
+		{Case: "", Args: []string{"", "AAA"}, HasError: false},
+	}
+
+	for _, tcase := range cases {
+		_, err := New("", tcase.Args...)
+
+		if tcase.HasError {
+			r.Error(err, tcase.Case)
+			continue
+		}
+
+		r.NoError(err, tcase.Case)
+	}
+}
