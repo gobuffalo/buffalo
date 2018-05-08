a212244daed9bf914498d6e5d639798c1292a3ae
Antonio Pagano <apagano@wawand.co>
2018-05-07 20:39:51 -0500

    adding cases names

--- generators/resource/generator_test.go
+++ generators/resource/generator_test.go
@@ -23,15 +23,15 @@ func Test_New_WithPropertyNames(t *testing.T) {
 		Args     []string
 		HasError bool
 	}{
-		{Case: "", Args: []string{"", "body,name"}, HasError: true},
-		{Case: "", Args: []string{"", "body,name:nulls.String"}, HasError: true},
-		{Case: "", Args: []string{"", "body", "name:nulls.String"}, HasError: false},
-		{Case: "", Args: []string{"", "body.name"}, HasError: true},
-		{Case: "", Args: []string{"", "body", "name"}, HasError: false},
-		{Case: "", Args: []string{"", "body-name"}, HasError: false},
-		{Case: "", Args: []string{"", "body_name"}, HasError: false},
-		{Case: "", Args: []string{"", "9A"}, HasError: true},
-		{Case: "", Args: []string{"", "AAA"}, HasError: false},
+		{Case: "original invalid", Args: []string{"", "body,name"}, HasError: true},
+		{Case: "original with type", Args: []string{"", "body,name:nulls.String"}, HasError: true},
+		{Case: "valid with type", Args: []string{"", "body", "name:nulls.String"}, HasError: false},
+		{Case: "invalid with dot", Args: []string{"", "body.name"}, HasError: true},
+		{Case: "valid", Args: []string{"", "body", "name"}, HasError: false},
+		{Case: "valid with separator", Args: []string{"", "body-name"}, HasError: false},
+		{Case: "valid", Args: []string{"", "body_name"}, HasError: false},
+		{Case: "invalid starting digit", Args: []string{"", "9A"}, HasError: true},
+		{Case: "valid uppercase", Args: []string{"", "AAA"}, HasError: false},
 	}
 
 	for _, tcase := range cases {
