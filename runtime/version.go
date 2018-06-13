c5f35a981943221a8b5c5ed753add896e2c3ef50
Mark Bates <mark@markbates.com>
2018-06-13 10:57:46 -0400

    reset back to development

--- runtime/version.go
+++ runtime/version.go
@@ -1,6 +1,5 @@
 package runtime
 
 // Version is the current version of the buffalo binary
-const Version = "v0.12.1"
-
-// const Version = "development"
+// const Version = "v0.12.1"
+const Version = "development"
