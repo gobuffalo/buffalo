6ecb5a7e00ad7aec94a6e24ba8d544e7accba86d
Mark Bates <mark@markbates.com>
2018-06-27 11:39:15 -0400

    fixed missing comment from merge

--- runtime/version.go
+++ runtime/version.go
@@ -3,4 +3,5 @@ package runtime
 // Version is the current version of the buffalo binary
 // const Version = "v0.12.3"
 
+// Version is the current version of the buffalo binary
 const Version = "development"
