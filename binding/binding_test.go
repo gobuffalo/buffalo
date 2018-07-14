b4add4f012cbb8c26224d706a27ea2b79f125cdd
Antonio Pagano <apagano@wawand.co>
2018-07-14 14:27:48 -0500

    prepending time formats when using RegisterTimeFormats

--- binding/binding_test.go
+++ binding/binding_test.go
@@ -20,3 +20,14 @@ func TestParseTime(t *testing.T) {
 	expected := time.Date(2017, time.January, 1, 0, 0, 0, 0, time.UTC)
 	r.Equal(expected, tt)
 }
+
+func TestParseTimeConflicting(t *testing.T) {
+	RegisterTimeFormats("2006-02-01")
+
+	r := require.New(t)
+	tt, err := parseTime([]string{"2017-01-10"})
+
+	r.NoError(err)
+	expected := time.Date(2017, time.October, 1, 0, 0, 0, 0, time.UTC)
+	r.Equal(expected, tt)
+}
