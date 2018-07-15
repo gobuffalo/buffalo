686cf12e54030e5f80e66f125d13e1346e6bf1ac
Mohammed Al Sahaf <msaa1990@gmail.com>
2018-07-15 19:25:10 +0300

    Add test cases for binding parseTime

--- binding/binding_test.go
+++ binding/binding_test.go
@@ -14,9 +14,36 @@ func TestParseTimeErrorParsing(t *testing.T) {
 }
 
 func TestParseTime(t *testing.T) {
+
 	r := require.New(t)
-	tt, err := parseTime([]string{"2017-01-01"})
-	r.NoError(err)
-	expected := time.Date(2017, time.January, 1, 0, 0, 0, 0, time.UTC)
-	r.Equal(expected, tt)
+
+	testCases := []struct {
+		input     string
+		expected  time.Time
+		expectErr bool
+	}{
+		{
+			input:     "2017-01-01",
+			expected:  time.Date(2017, time.January, 1, 0, 0, 0, 0, time.UTC),
+			expectErr: false,
+		},
+		{
+			input:     "2018-07-13T15:34",
+			expected:  time.Date(2018, time.July, 13, 15, 34, 0, 0, time.UTC),
+			expectErr: false,
+		},
+		{
+			input:     "2018-20-10T30:15",
+			expected:  time.Time{},
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		tt, err := parseTime([]string{tc.input})
+		if !tc.expectErr {
+			r.NoError(err)
+		}
+		r.Equal(tc.expected, tt)
+	}
 }
