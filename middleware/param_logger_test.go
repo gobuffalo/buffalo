2eff036e1f1157be6bc68ddbe496c82744f93d87
Antonio Pagano <apagano@wawand.co>
2018-08-02 09:56:00 -0500

    adding a bit more of tests

--- middleware/param_logger_test.go
+++ middleware/param_logger_test.go
@@ -4,6 +4,9 @@ import (
 	"net/url"
 	"testing"
 
+	"github.com/gobuffalo/buffalo"
+	"github.com/markbates/willie"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 )
 
@@ -55,4 +58,64 @@ func Test_maskSecretsCustom(t *testing.T) {
 	r.Equal(filteredForm.Get("password"), "Other")
 	r.Equal(filteredForm.Get("LastName"), filteredIndicator[0])
 	r.Equal(filteredForm.Get("SomeCVC"), "Untouched")
+}
+
+var lastEntry *logrus.Entry
+
+type testHook struct{}
+
+func (th testHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+func (th testHook) Fire(entry *logrus.Entry) error {
+	lastEntry = entry
+	return nil
+}
+
+type testLogger struct {
+	logrus.FieldLogger
+}
+
+func (l testLogger) WithField(s string, i interface{}) buffalo.Logger {
+	return testLogger{l.FieldLogger.WithField(s, i)}
+}
+
+func (l testLogger) WithFields(m map[string]interface{}) buffalo.Logger {
+	return testLogger{l.FieldLogger.WithFields(m)}
+}
+
+func newTestLogger() testLogger {
+	l := logrus.New()
+	l.AddHook(testHook{})
+	l.Level, _ = logrus.ParseLevel("debug")
+
+	return testLogger{l}
+}
+
+func Test_Logger(t *testing.T) {
+	r := require.New(t)
+	app := buffalo.New(buffalo.Options{})
+	app.Use(ParameterLogger)
+	app.Logger = newTestLogger()
+	emptyHandler := func(c buffalo.Context) error {
+		return nil
+	}
+
+	app.GET("/", emptyHandler)
+	app.POST("/", emptyHandler)
+
+	wi := willie.New(app)
+	wi.HTML("/?param=value").Get()
+
+	r.Contains(lastEntry.Data["params"], "{\"param\":[\"value\"]}")
+
+	wi.HTML("/").Post(url.Values{
+		"Password": []string{"123"},
+		"Name":     []string{"Antonio"},
+		"CVC":      []string{"123"},
+	})
+
+	r.Equal(lastEntry.Data["form"], "{\"CVC\":[\"[FILTERED]\"],\"Name\":[\"Antonio\"],\"Password\":[\"[FILTERED]\"]")
+
 }
