8096cc7868a0fe2c19787993455158879ca6b6e3
Stanislas Michalak <stanislas.michalak@gmail.com>
2018-08-03 16:01:14 +0200

    Fix missing "=" in content type middleware

--- middleware/content_type.go
+++ middleware/content_type.go
@@ -10,7 +10,7 @@ import (
 // override any content type sent by the client.
 //
 // Deprecated: use github.com/gobuffalo/mw-contenttype#Set instead.
-var SetContentType contenttype.Set
+var SetContentType = contenttype.Set
 
 // AddContentType will add a secondary content type to
 // a request. If no content type is sent by the client
@@ -18,7 +18,7 @@ var SetContentType contenttype.Set
 // content type will be used.
 //
 // Deprecated: use github.com/gobuffalo/mw-contenttype#Add instead.
-var AddContentType contenttype.Add
+var AddContentType = contenttype.Add
 
 func init() {
 	fmt.Printf("github.com/gobuffalo/buffalo/middleware is deprecated and will be removed in the next version. Please use github.com/gobuffalo/mw-contenttype, github.com/gobuffalo/mw-paramlogger and github.com/gobuffalo/mw-poptx instead.")
