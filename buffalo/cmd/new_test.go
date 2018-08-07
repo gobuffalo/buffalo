a9b9d4efa88cddb92e18f04254927ecd520b254e
Stanislas Michalak <stanislas.michalak@gmail.com>
2018-08-07 14:21:25 +0200

    Add nominal new app integration test

--- buffalo/cmd/new_test.go
+++ buffalo/cmd/new_test.go
@@ -2,9 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
 	"strings"
 	"testing"
 
+	"github.com/gobuffalo/envy"
 	"github.com/gobuffalo/pop"
 	"github.com/stretchr/testify/require"
 )
@@ -48,4 +52,36 @@ func Test_NewCmd_ForbiddenAppName(t *testing.T) {
 	})
 	err := c.Execute()
 	r.EqualError(err, "name buffalo is not allowed, try a different application name")
+}
+
+func Test_NewCmd_Nominal(t *testing.T) {
+	r := require.New(t)
+	c := RootCmd
+
+	tdir, err := ioutil.TempDir("", "gopath")
+	r.NoError(err)
+	defer os.RemoveAll(tdir)
+	gp, err := envy.MustGet("GOPATH")
+	r.NoError(err)
+	defer envy.MustSet("GOPATH", gp)
+	err = envy.MustSet("GOPATH", tdir)
+	r.NoError(err)
+
+	cpath := path.Join(tdir, "src", "github.com", "gobuffalo")
+	err = os.MkdirAll(cpath, 0700)
+	r.NoError(err)
+	pwd, err := os.Getwd()
+	r.NoError(err)
+	os.Chdir(cpath)
+	defer os.Chdir(pwd)
+
+	c.SetArgs([]string{
+		"new",
+		"hello_world",
+		"--skip-pop",
+		"--skip-webpack",
+	})
+	err = c.Execute()
+	r.NoError(err)
+
 }
