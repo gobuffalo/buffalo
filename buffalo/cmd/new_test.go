af83dbca0cf631b01ca7279782078b1d32695d1c
Stanislas Michalak <stanislas-m@users.noreply.github.com>
2018-08-08 20:52:54 +0200

    Update new project template to use extracted middlewares (#1213)
    * Update new project template to use extracted middlewares
    
    * Fix broken apiapp test
    
    * Fix ParameterLogger in app.go template
    
    * Improve GOPATH caching in integration tests
    
    * Create temp dir inside GOPATH instead of recreating the whole GOPATH.
    * Add new app as API test.

--- buffalo/cmd/new_test.go
+++ buffalo/cmd/new_test.go
@@ -58,21 +58,16 @@ func Test_NewCmd_Nominal(t *testing.T) {
 	r := require.New(t)
 	c := RootCmd
 
-	tdir, err := ioutil.TempDir("", "gopath")
-	r.NoError(err)
-	defer os.RemoveAll(tdir)
 	gp, err := envy.MustGet("GOPATH")
 	r.NoError(err)
-	defer envy.MustSet("GOPATH", gp)
-	err = envy.MustSet("GOPATH", tdir)
+	cpath := path.Join(gp, "src", "github.com", "gobuffalo")
+	tdir, err := ioutil.TempDir(cpath, "testapp")
 	r.NoError(err)
+	defer os.RemoveAll(tdir)
 
-	cpath := path.Join(tdir, "src", "github.com", "gobuffalo")
-	err = os.MkdirAll(cpath, 0700)
-	r.NoError(err)
 	pwd, err := os.Getwd()
 	r.NoError(err)
-	os.Chdir(cpath)
+	os.Chdir(tdir)
 	defer os.Chdir(pwd)
 
 	c.SetArgs([]string{
@@ -80,6 +75,33 @@ func Test_NewCmd_Nominal(t *testing.T) {
 		"hello_world",
 		"--skip-pop",
 		"--skip-webpack",
+		"--vcs=none",
+	})
+	err = c.Execute()
+	r.NoError(err)
+}
+
+func Test_NewCmd_API(t *testing.T) {
+	r := require.New(t)
+	c := RootCmd
+
+	gp, err := envy.MustGet("GOPATH")
+	r.NoError(err)
+	cpath := path.Join(gp, "src", "github.com", "gobuffalo")
+	tdir, err := ioutil.TempDir(cpath, "testapp")
+	r.NoError(err)
+	defer os.RemoveAll(tdir)
+
+	pwd, err := os.Getwd()
+	r.NoError(err)
+	os.Chdir(tdir)
+	defer os.Chdir(pwd)
+
+	c.SetArgs([]string{
+		"new",
+		"hello_world",
+		"--skip-pop",
+		"--api",
 		"--vcs=none",
 	})
 	err = c.Execute()
