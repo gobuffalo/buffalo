c4133486f3de72109ef1b0dd6215f03b15cbd21f
Stanislas Michalak <stanislas-m@users.noreply.github.com>
2018-08-13 23:24:21 +0200

    Add a build tag to guard integration tests (#1225)
    * Add a build tag to guard integration tests
    
    These tests require the tag "integration_test" now.
    
    * Fix filepath issue on Windows
    
    * Fix an issue when using --with-dep but dep is not there
    
    Due to the way exec.Command works, the command lookup was done before
    the generator execution, so even if we have a step to go get dep before
    running the goGet() step, it will always fallback to go get instead of
    using dep.
    
    This issue is only visible once, since it needs to have dep missing, which
    is installed during the first run.
    
    * Try to fix appveyor
    
    * WIP fixing Windows tests
    
    * [ci skip] WIP fix appveyor
    
    * WIP fix appveyor
    
    * Skip integration test with dep removed on Windows
    
    Due to the way Windows handles its environment, it can't see dep
    if we install it during the process. Preinstall dep using chocolatey.
    
    * Fix typo
    
    * Fix typo
    
    * Add more unit tests for app generator

--- generators/soda/soda.go
+++ generators/soda/soda.go
@@ -1,6 +1,8 @@
 package soda
 
 import (
+	"path/filepath"
+
 	"github.com/gobuffalo/makr"
 	"github.com/gobuffalo/pop/soda/cmd/generate"
 )
@@ -14,15 +16,15 @@ func (sd Generator) Run(root string, data makr.Data) error {
 		return sd.App.WithPop
 	}
 
-	f := makr.NewFile("models/models.go", nModels)
+	f := makr.NewFile(filepath.Join("models", "models.go"), nModels)
 	f.Should = should
 	g.Add(f)
 
-	f = makr.NewFile("models/models_test.go", nModelsTest)
+	f = makr.NewFile(filepath.Join("models", "models_test.go"), nModelsTest)
 	f.Should = should
 	g.Add(f)
 
-	f = makr.NewFile("grifts/db.go", nSeedGrift)
+	f = makr.NewFile(filepath.Join("grifts", "db.go"), nSeedGrift)
 	f.Should = should
 	g.Add(f)
 
