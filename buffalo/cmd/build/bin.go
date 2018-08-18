fd85b662913d47b38f7d1b2a9999188f855e00b5
Mark Bates <mark@markbates.com>
2018-08-18 13:00:00 -0400

    Revert "removed unused method"
    This reverts commit 6ee9369dd171366b33b4c89535f097e2c8ed20b9.

--- buffalo/cmd/build/bin.go
+++ buffalo/cmd/build/bin.go
@@ -1,12 +1,35 @@
 package build
 
 import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/gobuffalo/envy"
 	"github.com/pkg/errors"
 )
+
+func version() (string, string) {
+	_, err := exec.LookPath("git")
+	buildTime := fmt.Sprintf("\"%s\"", time.Now().Format(time.RFC3339))
+	version := buildTime
+	if err == nil {
+		cmd := exec.Command("git", "rev-parse", "--short", "HEAD")
+		out := &bytes.Buffer{}
+		cmd.Stdout = out
+		cmd.Stderr = os.Stderr
+		cmd.Stdin = os.Stdin
+		err = cmd.Run()
+		if err == nil && out.String() != "" {
+			version = strings.TrimSpace(out.String())
+		}
+	}
+	return version, buildTime
+}
 
 func (b *Builder) buildBin() error {
 	buildArgs := []string{"build", "-i"}
@@ -19,7 +42,11 @@ func (b *Builder) buildBin() error {
 
 	buildArgs = append(buildArgs, "-o", b.Bin)
 
-	flags := []string{}
+	version, buildTime := version()
+	flags := []string{
+		fmt.Sprintf("-X main.BuildVersion=%s", version),
+		fmt.Sprintf("-X main.BuildTime=%s", buildTime),
+	}
 
 	if b.Static {
 		flags = append(flags, "-linkmode external", "-extldflags \"-static\"")
@@ -35,9 +62,8 @@ func (b *Builder) buildBin() error {
 		}
 		flags = append(flags, b.LDFlags)
 	}
-	if len(flags) > 0 {
-		buildArgs = append(buildArgs, "-ldflags", strings.Join(flags, " "))
-	}
+
+	buildArgs = append(buildArgs, "-ldflags", strings.Join(flags, " "))
 
 	return b.exec(envy.Get("GO_BIN", "go"), buildArgs...)
 }
