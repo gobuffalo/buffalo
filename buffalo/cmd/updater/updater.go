589c3da9396247d37a0cfdb25eba37f2ec8dafd3
Mark Bates <mark@markbates.com>
2018-08-22 16:08:39 -0400

    aliases and deprecates the updater package in favor of the fix package

---
+++ buffalo/cmd/updater/updater.go
@@ -0,0 +1,7 @@
+package updater
+
+import "fmt"
+
+func init() {
+	fmt.Printf("github.com/gobuffalo/buffalo/updater is deprecated and will be removed in the next version. Please use github.com/gobuffalo/buffalo/fix instead.")
+}
