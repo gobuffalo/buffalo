fff825426be842bf8e6883cfb6d61c2a6b0b0765
Antonio Pagano <apagano@wawand.co>
2018-10-06 11:26:52 -0500

    vadding version.go changes

--- runtime/version.go
+++ runtime/version.go
@@ -1,10 +1,4 @@
 package runtime
 
 // Version is the current version of the buffalo binary
-<<<<<<< HEAD
 const Version = "development"
-=======
-const Version = "v0.12.7"
-
-// const Version = "development"
->>>>>>> master
