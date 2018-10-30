e9fe6e9ad6ec1d2ec7ea8eaeb6adc5f5aa92ef5e
Mark Bates <mark@markbates.com>
2018-10-30 12:37:33 -0400

    adds build events

---
+++ genny/build/events.go
@@ -0,0 +1,10 @@
+package build
+
+const (
+	// EvtBuildStart is emitted when building starts
+	EvtBuildStart = "buffalo:build:start"
+	// EvtBuildStop is emitted when building stops
+	EvtBuildStop = "buffalo:build:stop"
+	// EvtBuildStopErr is emitted when building stops
+	EvtBuildStopErr = "buffalo:build:stop:err"
+)
--- genny/build/validate.go
+++ genny/build/validate.go
@@ -1,13 +1,17 @@
 package build
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"strings"
 
 	"github.com/gobuffalo/genny"
+	"github.com/gobuffalo/meta"
 	"github.com/gobuffalo/packd"
 	"github.com/gobuffalo/plush"
+	"github.com/karrick/godirwalk"
 	"github.com/markbates/safe"
 	"github.com/pkg/errors"
 )
@@ -20,7 +24,7 @@ type TemplateValidator func(f genny.File) error
 // ValidateTemplates returns a genny.RunFn that will walk the
 // given box and run each of the files found through each of the
 // template validators
-func ValidateTemplates(box packd.Walkable, tvs []TemplateValidator) genny.RunFn {
+func ValidateTemplates(walk packd.Walkable, tvs []TemplateValidator) genny.RunFn {
 	if len(tvs) == 0 {
 		return func(r *genny.Runner) error {
 			return nil
@@ -28,7 +32,7 @@ func ValidateTemplates(box packd.Walkable, tvs []TemplateValidator) genny.RunFn 
 	}
 	return func(r *genny.Runner) error {
 		var errs []string
-		box.Walk(func(path string, file packd.File) error {
+		walk.Walk(func(path string, file packd.File) error {
 			info, err := file.FileInfo()
 			if err != nil {
 				return errors.WithStack(err)
@@ -78,4 +82,44 @@ func GoTemplateValidator(f genny.File) error {
 	t := template.New(f.Name())
 	_, err := t.Parse(f.String())
 	return err
+}
+
+type dirWalker struct {
+	dir string
+}
+
+func (d dirWalker) WalkPrefix(pre string, fn packd.WalkFunc) error {
+	return d.Walk(func(path string, file packd.File) error {
+		if strings.HasPrefix(path, pre) {
+			return fn(path, file)
+		}
+		return nil
+	})
+}
+
+func (d dirWalker) Walk(fn packd.WalkFunc) error {
+	callback := func(path string, de *godirwalk.Dirent) error {
+		if de != nil && de.IsDir() {
+			return nil
+		}
+		b, err := ioutil.ReadFile(path)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		f, err := packd.NewFile(path, bytes.NewReader(b))
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		return fn(path, f)
+	}
+
+	godirwalk.Walk(d.dir, &godirwalk.Options{
+		FollowSymbolicLinks: true,
+		Callback:            callback,
+	})
+	return nil
+}
+
+func templateWalker(app meta.App) packd.Walkable {
+	return dirWalker{dir: app.Root}
 }
