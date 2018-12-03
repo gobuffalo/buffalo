ec163da67dce63f2526747f3495fca5217fd92c7
Mark Bates <mark@markbates.com>
2018-12-03 14:05:01 -0500

    wip test

--- genny/resource/resource_test.go
+++ genny/resource/resource_test.go
@@ -1,28 +1,54 @@
 package resource
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/gobuffalo/genny/gentest"
+	packr "github.com/gobuffalo/packr/v2"
 	"github.com/stretchr/testify/require"
 )
 
+var coke = packr.New("coke", "./_fixtures")
+
+type pass struct {
+	Name  string
+	Args  []string
+	Flags []string
+}
+
 func Test_New(t *testing.T) {
-	r := require.New(t)
 
-	g, err := New(&Options{})
-	r.NoError(err)
+	table := []pass{
+		{"default", []string{"widget", "name", "desc:nulls.Text"}, []string{}},
+		{"nested", []string{"admin/widget", "name", "desc:nulls.Text"}, []string{}},
+		{"deep_nested", []string{"deep/admin/widget", "name", "desc:nulls.Text"}, []string{}},
+		{"skip_migration", []string{"widget", "name", "desc:nulls.Text"}, []string{"--skip-migration"}},
+		{"skip_model", []string{"widget", "name", "desc:nulls.Text"}, []string{"--skip-model"}},
+		{"use_model", []string{"widget", "name", "desc:nulls.Text"}, []string{"--use-model", "gadget"}},
+	}
 
-	run := gentest.NewRunner()
-	run.With(g)
+	for _, tt := range table {
+		t.Run(tt.Name, func(st *testing.T) {
+			r := require.New(st)
+			g, err := New(&Options{})
+			r.NoError(err)
 
-	r.NoError(run.Run())
+			run := gentest.NewRunner()
+			run.With(g)
 
-	res := run.Results()
+			r.NoError(run.Run())
 
-	r.Len(res.Commands, 0)
-	r.Len(res.Files, 1)
+			res := run.Results()
 
-	f := res.Files[0]
-	r.Equal("example.txt", f.Name())
+			r.Len(res.Commands, 0)
+
+			box := packr.New(tt.Name, filepath.Join("_fixtures", tt.Name))
+			r.Len(res.Files, len(box.List()))
+
+			f := res.Files[0]
+			r.Equal("example.txt", f.Name())
+
+		})
+	}
 }
