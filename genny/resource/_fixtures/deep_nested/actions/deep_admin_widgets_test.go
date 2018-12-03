841a9d9dde9cdf0d5581fae94fe916297cecb915
Mark Bates <mark@markbates.com>
2018-12-03 13:51:10 -0500

    generated some fixtures

---
+++ genny/resource/_fixtures/deep_nested/actions/deep_admin_widgets_test.go
@@ -0,0 +1,29 @@
+package actions
+
+func (as *ActionSuite) Test_DeepAdminWidgetsResource_List() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_DeepAdminWidgetsResource_Show() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_DeepAdminWidgetsResource_New() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_DeepAdminWidgetsResource_Create() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_DeepAdminWidgetsResource_Edit() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_DeepAdminWidgetsResource_Update() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_DeepAdminWidgetsResource_Destroy() {
+	as.Fail("Not Implemented!")
+}
