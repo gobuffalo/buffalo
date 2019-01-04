d413e53b31b558f282e48329202b933c795a7370
Mark Bates <mark@markbates.com>
2019-01-04 17:37:53 -0500

    pickles

--- it.sh
+++ it.sh
@@ -6,7 +6,7 @@ set -ex
 BP=$GOPATH/src/github.com/gobuffalo/buffalo
 
 go get -u -v github.com/gobuffalo/logger
-go get -u -v ./...
+make ci-deps
 
 gometalinter --vendor --deadline=5m ./... --skip=internal
 make install
