a66dae24a2b14d6455b7474e34cfd9a59354b614
Mark Bates <mark@markbates.com>
2019-01-17 12:26:53 -0500

    wip

---
+++ buffalo/cmd/internal/integration/integration_test.go
@@ -0,0 +1,47 @@
+package integration
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/gobuffalo/buffalo/buffalo/cmd"
+	"github.com/gobuffalo/envy"
+	"github.com/markbates/safe"
+)
+
+func call(args []string, fn func() error) error {
+	gp, err := envy.MustGet("GOPATH")
+	if err != nil {
+		return err
+	}
+	if fn == nil {
+		if err := exec(args); err != nil {
+			return err
+		}
+	}
+	cpath := filepath.Join(gp, "src", "github.com", "gobuffalo")
+	tdir, err := ioutil.TempDir(cpath, "testapp")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tdir)
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	os.Chdir(tdir)
+	defer os.Chdir(pwd)
+
+	if err := exec(args); err != nil {
+		return err
+	}
+	return safe.RunE(fn)
+}
+
+func exec(args []string) error {
+	c := cmd.RootCmd
+	c.SetArgs(args)
+	return c.Execute()
+}
--- buffalo/cmd/internal/integration/new_test.go
+++ buffalo/cmd/internal/integration/new_test.go
@@ -1,6 +1,6 @@
 // +build integration_test
 
-package cmd
+package integration
 
 import (
 	"fmt"
@@ -16,36 +16,33 @@ import (
 )
 
 func Test_NewCmd_NoName(t *testing.T) {
+	err := call([]string{"new"}, nil)
 	r := require.New(t)
-	c := RootCmd
-	c.SetArgs([]string{
-		"new",
-	})
-	err := c.Execute()
+	r.Error(err)
 	r.EqualError(err, "you must enter a name for your new application")
 }
 
 func Test_NewCmd_InvalidDBType(t *testing.T) {
-	r := require.New(t)
-	c := RootCmd
-	c.SetArgs([]string{
+	args := []string{
 		"new",
 		"coke",
 		"--db-type",
 		"a",
-	})
-	err := c.Execute()
+	}
+	err := call(args, nil)
+	r := require.New(t)
+	r.NoError(err)
 	r.EqualError(err, fmt.Sprintf(`unknown dialect "a" expecting one of %s`, strings.Join(pop.AvailableDialects, ", ")))
 }
 
 func Test_NewCmd_ForbiddenAppName(t *testing.T) {
-	r := require.New(t)
-	c := RootCmd
-	c.SetArgs([]string{
+	args := []string{
 		"new",
 		"buffalo",
-	})
-	err := c.Execute()
+	}
+	err := call(args, nil)
+	r := require.New(t)
+	r.NoError(err)
 	r.EqualError(err, "name buffalo is not allowed, try a different application name")
 }
 
