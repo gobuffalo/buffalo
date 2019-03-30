c18a924c41e7717551f064591a63a8bd5d174d71
Mark Bates <mark@markbates.com>
2019-03-30 15:43:59 -0400

    cleaned up config/* output

--- genny/info/config.go
+++ genny/info/config.go
@@ -1,9 +1,7 @@
 package info
 
 import (
-	"fmt"
 	"path/filepath"
-	"strings"
 
 	"github.com/gobuffalo/genny"
 	"github.com/gobuffalo/packd"
@@ -21,12 +19,9 @@ func configs(opts *Options, box ListWalker) genny.RunFn {
 		if len(box.List()) == 0 {
 			return nil
 		}
-		opts.Out.Header("Buffalo: Config Files")
 		return box.Walk(func(path string, f packr.File) error {
-			p := strings.TrimPrefix(path, opts.App.Root)
-			p = strings.TrimPrefix(p, string(filepath.Separator))
-			opts.Out.WriteString(fmt.Sprintf("\n### %s\n", p))
-			opts.Out.WriteString(f.String())
+			opts.Out.Header("Buffalo: " + filepath.Join("config", path))
+			opts.Out.WriteString(f.String() + "\n")
 			return nil
 		})
 	}
