dcd36c27e0f51e938308fcf559fd85cb981c00b0
Mark Bates <mark@markbates.com>
2019-05-26 17:01:05 -0400

    Update options.go

--- genny/build/options.go
+++ genny/build/options.go
@@ -53,7 +53,6 @@ func (opts *Options) Validate() error {
 	if opts.App.IsZero() {
 		opts.App = meta.New(pwd)
 	}
-	fmt.Printf("### opts.App (%T) -> %v\n", opts.App, opts.App)
 	if len(opts.Environment) == 0 {
 		opts.Environment = "development"
 	}
