87da0cc75eb81991b9ffc56cb894be0aa256b373
Robin Gögge <r.goegge@outlook.com>
2019-10-22 15:55:03 +0200

    Add --json flag to buffalo version command
    Resolves: #1788

--- buffalo/cmd/version.go
+++ buffalo/cmd/version.go
@@ -1,13 +1,20 @@
 package cmd
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/gobuffalo/buffalo/runtime"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
+var jsonOutput bool
+
 func init() {
 	decorate("version", versionCmd)
+	versionCmd.Flags().BoolVar(&jsonOutput, "json", false, "Print information in json format")
+
 	RootCmd.AddCommand(versionCmd)
 }
 
@@ -15,6 +22,15 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version information",
 	Run: func(c *cobra.Command, args []string) {
+		if jsonOutput {
+			build := runtime.BuildInfo{}
+			build.Version = runtime.Version
+			enc := json.NewEncoder(os.Stderr)
+			enc.SetIndent("", "    ")
+			enc.Encode(build)
+			return
+		}
+
 		logrus.Infof("Buffalo version is: %s", runtime.Version)
 	},
 	// needed to override the root level pre-run func
--- buffalo/cmd/version_test.go
+++ buffalo/cmd/version_test.go
@@ -16,3 +16,15 @@ func Test_VersionCmd(t *testing.T) {
 	err := c.Execute()
 	r.NoError(err)
 }
+
+func Test_VersionJSONCmd(t *testing.T) {
+	r := require.New(t)
+
+	c := RootCmd
+	c.SetArgs([]string{
+		"version",
+		"--json",
+	})
+	err := c.Execute()
+	r.NoError(err)
+}
