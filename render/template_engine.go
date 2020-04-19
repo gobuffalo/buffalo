3d85cfb53944a7a43f10a4dca7f9b529546c792e
Antonio Pagano <645522+paganotoni@users.noreply.github.com>
2020-04-19 10:43:34 -0500

    removing helpers from the Go template Engine (#1971)

--- render/template_engine.go
+++ render/template_engine.go
@@ -10,15 +10,7 @@ type TemplateEngine func(input string, data map[string]interface{}, helpers map[
 
 // GoTemplateEngine implements the TemplateEngine interface for using standard Go templates
 func GoTemplateEngine(input string, data map[string]interface{}, helpers map[string]interface{}) (string, error) {
-	// since go templates don't have the concept of an optional map argument like Plush does
-	// add this "null" map so it can be used in templates like this:
-	// {{ partial "flash.html" .nilOpts }}
-	data["nilOpts"] = map[string]interface{}{}
-
 	t := template.New(input)
-	if helpers != nil {
-		t = t.Funcs(helpers)
-	}
 
 	t, err := t.Parse(input)
 	if err != nil {
