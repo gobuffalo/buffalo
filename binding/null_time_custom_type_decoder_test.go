8a13fdd412f685b666141c60aef78ca6bd46c940
Antonio Pagano <apagano@wawand.co>
2020-04-20 14:42:58 -0500

    adding tests for null time decoder

--- binding/null_time_custom_type_decoder_test.go
+++ binding/null_time_custom_type_decoder_test.go
@@ -1,20 +1,57 @@
 package binding
 
-import "github.com/gobuffalo/nulls"
+import (
+	"testing"
+	"time"
 
-type NullTimeCustomTypeDecoder struct {
-	*TimeCustomTypeDecoder
-}
+	"github.com/gobuffalo/nulls"
+	"github.com/stretchr/testify/require"
+)
 
-func (td NullTimeCustomTypeDecoder) Decode(values []string) (interface{}, error) {
-	var ti nulls.Time
+func Test_NullTimeCustomDecoder_Decode(t *testing.T) {
+	r := require.New(t)
 
-	t, err := td.parseTime(values)
-	if err != nil {
-		return ti, err
+	timeCustom := TimeCustomTypeDecoder{
+		formats: &RequestBinder.timeFormats,
 	}
-	ti.Time = t
-	ti.Valid = true
 
-	return ti, nil
+	nullTimeCustom := NullTimeCustomTypeDecoder{&timeCustom}
+
+	testCases := []struct {
+		input     string
+		expected  time.Time
+		expectErr bool
+	}{
+		{
+			input:    "2017-01-01",
+			expected: time.Date(2017, time.January, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			input:    "2018-07-13T15:34",
+			expected: time.Date(2018, time.July, 13, 15, 34, 0, 0, time.UTC),
+		},
+		{
+			input:    "2018-20-10T30:15",
+			expected: time.Time{},
+		},
+		{
+			input:    "",
+			expected: time.Time{},
+		},
+	}
+
+	for index, testCase := range testCases {
+
+		tt, err := nullTimeCustom.Decode([]string{testCase.input})
+		r.IsType(tt, nulls.Time{})
+		nt := tt.(nulls.Time)
+
+		if testCase.expectErr {
+			r.Error(err)
+			r.Equal(nt.Valid, false)
+			continue
+		}
+
+		r.Equal(testCase.expected, nt.Time)
+	}
 }
--- binding/request_binder_test.go
+++ binding/request_binder_test.go
@@ -1,1 +1,12 @@
 package binding
+
+// func TestParseTimeConflicting(t *testing.T) {
+// 	RegisterTimeFormats("2006-02-01")
+
+// 	r := require.New(t)
+// 	tt, err := parseTime([]string{"2017-01-10"})
+
+// 	r.NoError(err)
+// 	expected := time.Date(2017, time.October, 1, 0, 0, 0, 0, time.UTC)
+// 	r.Equal(expected, tt)
+// }
