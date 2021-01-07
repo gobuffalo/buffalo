b83ed9e6aae21d010f172db620106cf83389ad49
Antonio Pagano <apagano@wawand.co>
2021-01-07 17:59:38 -0500

    allowing apps to define their own route namers

--- app.go
+++ app.go
@@ -23,6 +23,10 @@ type App struct {
 	root          *App
 	children      []*App
 	filepaths     []string
+
+	// Routenamer for the app this could be used to override for application
+	// specific automatic route naming.
+	RouteNamer RouteNamer
 }
 
 // Muxer returns the underlying mux router to allow
@@ -48,6 +52,8 @@ func New(opts Options) *App {
 		moot:     &sync.RWMutex{},
 		routes:   RouteList{},
 		children: []*App{},
+
+		RouteNamer: baseRouteNamer{},
 	}
 
 	dem := a.defaultErrorMiddleware
