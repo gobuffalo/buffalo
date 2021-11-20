4644eccd2946d33754c303e0ef8b02c199997b60
Matthias Fasching <fasching.matthias@gmail.com>
2021-11-20 16:34:08 +0000

    Improve performance of WrapBuffaloHandler (#2146)
    * Add Benchmark to test performance improvements
    
    * Implement performance improvements
    
    * Minor improvements to default context
    
    * Integrate wrapcontext functionality into defaultcontext

--- go.mod
+++ go.mod
@@ -5,32 +5,31 @@ go 1.16
 require (
 	github.com/BurntSushi/toml v0.4.1
 	github.com/dustin/go-humanize v1.0.0
-	github.com/fatih/color v1.12.0
+	github.com/fatih/color v1.13.0
 	github.com/gobuffalo/envy v1.10.1
-	github.com/gobuffalo/events v1.4.1
+	github.com/gobuffalo/events v1.4.2
 	github.com/gobuffalo/flect v0.2.4
-	github.com/gobuffalo/github_flavored_markdown v1.1.0
-	github.com/gobuffalo/helpers v0.6.2
-	github.com/gobuffalo/httptest v1.5.0
-	github.com/gobuffalo/logger v1.0.4
+	github.com/gobuffalo/github_flavored_markdown v1.1.1
+	github.com/gobuffalo/helpers v0.6.3
+	github.com/gobuffalo/httptest v1.5.1
+	github.com/gobuffalo/logger v1.0.6
 	github.com/gobuffalo/meta v0.3.1
-	github.com/gobuffalo/nulls v0.4.0
-	github.com/gobuffalo/packd v1.0.0
+	github.com/gobuffalo/nulls v0.4.1
+	github.com/gobuffalo/packd v1.0.1
 	github.com/gobuffalo/packr/v2 v2.8.1
-	github.com/gobuffalo/plush/v4 v4.1.6
+	github.com/gobuffalo/plush/v4 v4.1.8
 	github.com/gobuffalo/pop/v5 v5.3.4
-	github.com/gobuffalo/tags/v3 v3.1.0
+	github.com/gobuffalo/tags/v3 v3.1.1
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/sessions v1.2.1
 	github.com/karrick/godirwalk v1.16.1
 	github.com/markbates/grift v1.5.0
 	github.com/markbates/oncer v1.0.0
-	github.com/markbates/refresh v1.11.1
+	github.com/markbates/refresh v1.12.0
 	github.com/markbates/safe v1.0.1
 	github.com/markbates/sigtx v1.0.0
-	github.com/microcosm-cc/bluemonday v1.0.16 // indirect
-	github.com/monoculum/formam v0.0.0-20210523135142-1af3317b7b9b
+	github.com/monoculum/formam v3.5.5+incompatible
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.2.1
 	github.com/stretchr/testify v1.7.0
--- wrappers.go
+++ wrappers.go
@@ -1,6 +1,13 @@
 package buffalo
 
-import "net/http"
+import (
+	"net/http"
+	"net/url"
+	"sync"
+
+	"github.com/gobuffalo/buffalo/internal/httpx"
+	"github.com/gorilla/mux"
+)
 
 // WrapHandler wraps a standard http.Handler and transforms it
 // into a buffalo.Handler.
@@ -17,19 +24,64 @@ func WrapHandlerFunc(h http.HandlerFunc) Handler {
 	return WrapHandler(h)
 }
 
-// WrapBuffaloHandler wraps a buffalo.Handler to
-// standard http.Handler
+// WrapBuffaloHandler wraps a buffalo.Handler to a standard http.Handler
+//
+// NOTE: A buffalo Handler expects a buffalo Context. WrapBuffaloHandler uses
+// the same logic as DefaultContext where possible, but some functionality
+// (e.g. sessions and logging) WILL NOT work with this unwrap function. If
+// those features are needed a custom UnwrapHandlerFunc needs to be
+// implemented that provides a Context implementing those features.
 func WrapBuffaloHandler(h Handler) http.Handler {
-	a := New(Options{})
-	// it doesn't matter what we actually map it
-	// GET, POST, etc... we just need the underlying
-	// RouteInfo, which implements http.Handler
-	ri := a.GET("/", h)
-	return ri
+	return WrapBuffaloHandlerFunc(h)
 }
 
-// WrapBuffaloHandlerFunc wraps a buffalo.Handler to
-// standard http.HandlerFunc
+// WrapBuffaloHandlerFunc wraps a buffalo.Handler to a standard http.HandlerFunc
+//
+// NOTE: A buffalo Handler expects a buffalo Context. WrapBuffaloHandlerFunc uses
+// the same logic as DefaultContext where possible, but some functionality
+// (e.g. sessions and logging) WILL NOT work with this unwrap function. If
+// those features are needed a custom WrapBuffaloHandlerFunc needs to be
+// implemented that provides a Context implementing those features.
 func WrapBuffaloHandlerFunc(h Handler) http.HandlerFunc {
-	return WrapBuffaloHandler(h).ServeHTTP
+	return func(res http.ResponseWriter, req *http.Request) {
+		if ws, ok := res.(*Response); ok {
+			res = ws
+		}
+
+		// Parse URL Params
+		params := url.Values{}
+		vars := mux.Vars(req)
+		for k, v := range vars {
+			params.Add(k, v)
+		}
+
+		// Parse URL Query String Params
+		// For POST, PUT, and PATCH requests, it also parse the request body as a form.
+		// Request body parameters take precedence over URL query string values in params
+		if err := req.ParseForm(); err == nil {
+			for k, v := range req.Form {
+				for _, vv := range v {
+					params.Add(k, vv)
+				}
+			}
+		}
+
+		ct := httpx.ContentType(req)
+
+		data := &sync.Map{}
+		data.Store("current_path", req.URL.Path)
+		data.Store("contentType", ct)
+		data.Store("method", req.Method)
+
+		c := &DefaultContext{
+			Context:     req.Context(),
+			contentType: ct,
+			response:    res,
+			request:     req,
+			params:      params,
+			flash:       &Flash{data: map[string][]string{}},
+			data:        data,
+		}
+		h(c)
+	}
 }
