441b07f8da9aec77e8c86f4d99d0303ec22499a0
Matthias Fasching <Matthias.Fasching@zuehlke.com>
2021-11-21 13:12:19 +0000

    Remove obsolete fields

--- render/options.go
+++ render/options.go
@@ -17,16 +17,8 @@ type Options struct {
 	// JavaScriptLayout is the default layout to be used with all JavaScript renders.
 	JavaScriptLayout string
 
-	// TemplatesBox is the location of the templates directory on disk.
-	// Deprecated: use TemplateFS instead
-	// TemplatesBox packd.Box
-
 	// TemplateFS is the fs.FS that holds the templates
 	TemplatesFS fs.FS
-
-	// AssetsBox is the location of the public assets the app will serve.
-	// Deprecated: use AssetsFS instead
-	// AssetsBox packd.Box
 
 	// AssetsFS is the fs.FS that holds the of the public assets the app will serve.
 	AssetsFS fs.FS
