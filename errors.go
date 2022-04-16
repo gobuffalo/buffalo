1acf05b3e1af3a2e28bd5a8bbba93127ed0f9a8e
Stefan Aurori <aurori@gmail.com>
2022-04-16 11:00:02 -0400

    HTTPError Error() return  when Cause is nil

--- errors.go
+++ errors.go
@@ -30,7 +30,10 @@ func (h HTTPError) Unwrap() error {
 
 // Error returns the cause of the error as string.
 func (h HTTPError) Error() string {
-	return fmt.Sprint(h.Cause)
+	if h.Cause != nil {
+		return h.Cause.Error()
+	}
+	return "unknown cause"
 }
 
 // ErrorHandler interface for handling an error for a
